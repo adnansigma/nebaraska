01e9bc22172873e964db81ade730adcdad4d5c2a
Aleena Dilawaiz <alinadilawaiz21@gmail.com>
2026-04-04 13:42:57 +0500

    updated db

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,11 +1,8 @@
 import { Pool } from 'pg'
 
 const pool = new Pool({
-    host    : 'localhost',
-    port    : 5432,
-    database: 'nebaraska',       // your db name
-    user    : 'postgres',           // your pgAdmin username
-    password: 'alina_neb_098_123098', // your pgAdmin password
+    connectionString: process.env.POSTGRES_URL_NON_POOLING,
+    ssl: { rejectUnauthorized: false }
 })
 
 export default pool
