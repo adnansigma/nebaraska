ff70b4c0a1f66c6e9a1bb7a3396506550e48d5e5
Aleena Dilawaiz <alinadilawaiz21@gmail.com>
2026-05-10 13:37:43 +0500

    refactor(db): update database connection configuration for improved security

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,20 +1,8 @@
-// import { Pool } from 'pg'
-
-// const pool = new Pool({
-//     connectionString: process.env.DATABASE_URL || process.env.POSTGRES_URL_NON_POOLING,
-//     ssl: { rejectUnauthorized: false }
-// })
-
-// export default pool
-
 import { Pool } from 'pg'
 
 const pool = new Pool({
-    host    : 'localhost',
-    port    : 5432,
-    database: 'nebaraska',       // your db name
-    user    : 'postgres',           // your pgAdmin username
-    password: 'alina_neb_098_123098', // your pgAdmin password
+    connectionString: process.env.DATABASE_URL || process.env.POSTGRES_URL_NON_POOLING,
+    ssl: { rejectUnauthorized: false }
 })
 
 export default pool
